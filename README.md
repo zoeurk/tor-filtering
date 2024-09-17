d7c81c6117f9043d159fa24ecbfb167bf345d3c5
zoeurk <zoeurk@gmail.com>
2024-09-17 11:15:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Filtering The Network Coming From Tor
+## Configure your firewall (I use nftables)
+It's looks like who Tor Network have a very small TTL (less than 63)
+So we need to create a rules like:  
+iifname $interface ct state new tcp dport 80 ip ttl < 63 log prefix "Tor User: " flags all  
+iifname $interface ct state new tcp dport 80 ip ttl < 63 log accept  
+Of course you can simply drop it, or redirect it to another service.  
+  
+## Configure your logs
+For have a separate log for this rule we need to configure rsyslog  
+Somthing like that work for me:  
+msg,contains,"Tor Network:"  
+*.* /var/log/nftables-tor-access.log
+
+## How to see the TTL
+You can use tcpdump (/or tying to log with your firewall) for see the TTL  
+On the loopback your website have a better response  
+After access your website with network  
+For finish access your site with Tor  
+You can notice the TTL is different.  
+It's like that who I have see how to filtering Tor Network  
+  
+I give you somthing to try  
+You should modify it.  
+You can integrate it on your site.  
+Maybe Developpers, systems administrators (,...)  
+have to discuss this among themselves.
---
+++ codes/php/tor.php
@@ -0,0 +1,47 @@
+<?php
+	exec(". /opt/get-ip.src", $msg, $ret);
+	if($msg[0] == "Services"){
+		$toronion = $msg[0];
+	}else{
+		if($_SERVER['REMOTE_ADDR'] != "127.0.0.1"){
+			exec("/usr/bin/wget --quiet -O /tmp/search.json https://onionoo.torproject.org/details?search=" . $msg[0], $output, $ipret);
+			$file = file_get_contents("/tmp/search.json");
+			$json = json_decode($file);
+			foreach($json->relays[0]->exit_addresses as $val){
+				if($msg[0] == $val){
+					$tor = file_get_contents("/var/opt/tor/service/hostname");
+					$toronion = str_replace(PHP_EOL, '', $tor);
+					break;
+				}
+			}
+		}else{
+			$toronion = "Services";
+		}
+	}
+?>
+<html lang="fr">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="shortcut icon" href="/www/html/public/images/mush.ico" sizes="32x32"/>
+		<title>Zoeurk JukeBox</title>
+	</head>
+	<body>
+		<?php
+			if($toronion != "Services"){
+				echo "<h3>Access by Tor</h3>";
+				echo "<h4>It looks like what you are using Tor</h4>Your ip is: <b style='color:green'>" . $msg[0] . "</b><br>And your country is: <b style='color:green'>" . $json->relays[0]->country_name . "</b><br>You while be redirected in few seconds to:<i style='color:green'>" . $tor . "</i><br><H6>If the link don't appear</h6>Try to change your exit node :/<br>Or try <b><a href='http://zoeurk.freeddns.org/tor_onion_router/'>zoeurkJukeBox</a></b>";
+			}
+		?>
+	</body>
+	<script type="text/javascript">
+		var url = <?php echo '"' . $toronion . '"';?>;
+		if(url != "Services"){
+			setTimeout(function(){window.location.replace("http://" + url);}, 2250);
+		}
+	</script>
+</html>
+<?php
+	if($toronion != "Services"){
+		exit;
+	}
+?>
